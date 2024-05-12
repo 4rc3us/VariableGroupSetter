73903e0b6d6d8b98133110478788ebec8fa4b24c
JDaniel <juandanielarce398@gmail.com>
2024-05-12 07:47:21 -0500

    Fix inner objs

--- setVariables.py
+++ setVariables.py
@@ -3,14 +3,19 @@ import subprocess
 import sys
 
 def SetVariable(data, path=''):
-    for k,v in data.items():
-        new_key = f"{path}.{k}" if path else k
-        if isinstance(v, dict):
+    new_key = path
+    if isinstance(data, dict):
+        for k,v in data.items():
+            new_key = f"{path}.{k}" if path else k
             SetVariable(v, new_key)
-        else:
-            cmd = f"az pipelines variable-group variable create --detect false --group-id {group_id} --name {new_key} --value \"{v}\" --org https://dev.azure.com/CASProyectos/ --project \"OmniCanalidad y Cliente 360 - CRM\""
-            print(cmd)
-            subprocess.run(cmd, shell=True)
+    elif isinstance(data, list):
+        for i,v in enumerate(data):
+            new_key = f"{path}[{i}]" if path else f"[{i}]"
+            SetVariable(v, new_key)
+    else:
+        cmd = f"az pipelines variable-group variable create --detect false --group-id {group_id} --name {new_key} --value {json.dumps(data)} --org https://dev.azure.com/CASProyectos/ --project \"OmniCanalidad y Cliente 360 - CRM\""
+        print(cmd)
+        subprocess.run(cmd, shell=True)
 
 group_id = sys.argv[1]
 json_path = sys.argv[2]
